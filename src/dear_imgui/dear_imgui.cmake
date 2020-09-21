7ddbee50f53b3631e40291c74ae1909a27384ae0
surrealwaffle <aomaniac@gmail.com>
2020-09-21 02:20:41 -0700

    restoring lost commits: restructure dear imgui

--- src/dear_imgui/dear_imgui.cmake
+++ src/dear_imgui/dear_imgui.cmake
@@ -1,14 +1,17 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 add_library(dear_imgui STATIC
-    src/dear_imgui/imgui.cpp
-    src/dear_imgui/imgui_impl_dx9.cpp
-    src/dear_imgui/imgui_draw.cpp
-    src/dear_imgui/imgui_impl_dx9.cpp
-    src/dear_imgui/imgui_widgets.cpp
+    src/dear_imgui/src/imgui.cpp
+    src/dear_imgui/src/imgui_impl_dx9.cpp
+    src/dear_imgui/src/imgui_draw.cpp
+    src/dear_imgui/src/imgui_widgets.cpp
+    src/dear_imgui/src/imgui_demo.cpp
+)
+
+target_compile_options(dear_imgui
+    PRIVATE -Wno-old-style-cast
 )
 
 target_include_directories(dear_imgui
-    PRIVATE   src/dear_imgui/include/dear_imgui
-    INTERFACE src/dear_imgui/include
+    PUBLIC src/dear_imgui/include
 )
